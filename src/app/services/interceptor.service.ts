bd41eb8bbb849adc4c9ed119e97b475105d80bbc
Axel Corona <coronaaxel115@gmail.com>
2020-05-28 19:31:46 -0500

    fix

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -10,6 +10,7 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ClientesService } from './clientes.service';
+import { UsuariosService } from './usuarios.service';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -17,17 +18,21 @@ import { Router } from '@angular/router';
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor(private _clientes: ClientesService, private router: Router) { }
+  constructor(
+    private _usuarios: UsuariosService,
+    private _clientes: ClientesService,
+    private router: Router
+    ) { }
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     let request = req;
 
     // inyectar la llave en los headers de las solicitudes...
-    if (this._clientes.jwt) {
+    if (this._clientes.jwt || this._usuarios.jwt) {
       request = req.clone({
         setHeaders: {
-          authorization: 'Bearer ' + this._clientes.jwt,
+          authorization: 'Bearer ' + (this._clientes.jwt || this._usuarios.jwt),
         }
       });
     }
