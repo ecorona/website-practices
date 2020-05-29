6b9d160efd12f6238dcddd424508981d66db7aea
Erik Corona <ecorona@snell.com.mx>
2020-05-29 10:27:28 -0500

    borrar el cliente del array

--- src/app/components/admin/clientes/clientes.component.ts
+++ src/app/components/admin/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientesService } from 'src/app/services/clientes.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-clientes',
@@ -8,13 +9,13 @@ import { ClientesService } from 'src/app/services/clientes.service';
 })
 export class ClientesComponent implements OnInit{
 
-  clientes = []
+  clientes = [] //este array contiene todos los clientes
 
   constructor(
-    private _clientes: ClientesService,
+    private _clientes: ClientesService, //traemos el servicio para manipularlos
   ) {}
   ngOnInit(): void {
-    this.buscarClientes()
+    this.buscarClientes() //al inicio, traer los clientes
   }
  
    error = false;
@@ -24,11 +25,8 @@ export class ClientesComponent implements OnInit{
      this._clientes.buscarClientes().subscribe(resp =>{
       console.log("respueste de Get", resp );
       setTimeout(()=>{
-        this.clientes = resp
+        this.clientes = resp //insertarlos en el array
       })
-     
-      
-
 
      }, (err) => {
 
@@ -38,23 +36,25 @@ export class ClientesComponent implements OnInit{
      });
    }
 
-
    borrarCliente(cliente){
      console.log("borrando cliente", cliente);
      this._clientes.borrarCliente({
        id: cliente.id
-     }).subscribe(resp =>{
-       console.log("cliente eliminado", resp);
-       
-
-       
+     }).subscribe(clienteBorrado =>{
+       console.log("cliente eliminado", clienteBorrado);
+       if( clienteBorrado && clienteBorrado.id ){
+         // quitar del array de clientes, el que fue eliminado 
+         //buscar su indice en el array, por medio de su id
+         let idx = _.findIndex(this.clientes,{ id: clienteBorrado.id });
+         if( idx > -1 ){
+          setTimeout(()=>{
+            this.clientes.splice(idx,1);
+          })
+         }
+       }
      }, (err) => { //en caso de error en el http request
       //mostrar el mensaje de error (html) cambiando la variable.
-      
       console.log('error al borrar registro:', err);
      });
-     
    }
-   
-   
 }
